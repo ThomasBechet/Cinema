f5f436ab0dfa159d0a8dde63d985cb1a4f792bf9
Epulapp <epulapp@epulapp1913.univ-lyon1.fr>
2019-12-04 17:59:45 +0100

    code rework

--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -3,59 +3,93 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {Md5} from 'ts-md5/dist/md5';
 
+enum HttpRequestType {
+  POST,
+  GET
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class SessionService {
 
+  readonly KW_token = 'token';
+  readonly KW_url = "http://34.77.176.92/users/";
+  readonly KW_url_preconnect = this.KW_url + "preconnect";
+  readonly KW_url_connect = this.KW_url + "connect";
+  readonly KW_url_disconnect = this.KW_url + "disconnect";
+  readonly KW_url_register = this.KW_url + "add";
+  readonly KW_url_update = this.KW_url + "update";
+  readonly KW_errorMsg = 'errorMsg';
+  readonly KW_username = 'username';
+  readonly KW_email = 'email';
+
   constructor(private httpClient : HttpClient) { 
     
   }
 
+  private buildAuthentificationHeader(token) {
+    return new HttpHeaders({
+      'Authorization': "bearer " + token
+    });
+  }
+
+  private send(url, header, body, patch) {
+    const httpHeader = new HttpHeaders(header);
+    return new Promise((resolve, reject) => {
+      this.httpClient.post(url, body, {headers : httpHeader}).subscribe(
+        (response) => {
+          if(response.hasOwnProperty(this.KW_errorMsg)) {
+            reject(response[this.KW_errorMsg]);
+          } else {
+            resolve(response);
+          }
+        },
+        (error) => {
+          reject(error);
+        }
+      );
+    });
+  }
+
   preconnect() {
     return new Promise((resolve, reject) => {
-      let token = localStorage.getItem('token');
+      let token = localStorage.getItem(this.KW_token);
       if(token !== null) {
-        const url = "http://34.77.176.92/users/preconnect";
-        const header = new HttpHeaders({
-          'Authorization': "Bearer " + token
+        const header = this.buildAuthentificationHeader(token);
+        this.send(this.KW_url_preconnect, null, header, HttpRequestType.POST)
+        .then((response) => {
+          this.setUsername(response['user']['name']);
+          this.setEmail(response['user']['email']);
+          resolve(null);
+        })
+        .catch((error) => {
+          reject(error);
         });
-        this.httpClient.post(url, null, {headers: header}).subscribe(
-          (response) => {
-            if(response['auth'] === true) {
-              let username = response['user']['name'];
-              let email = response['user']['email'];
-              localStorage.setItem('username', username);
-              localStorage.setItem('email', email);
-              console.log("successfully preconnected");
-              resolve();
-            } else {
-              localStorage.removeItem('token');
-              reject();
-            }
-          },
-          (error) => {
-            console.log("failed to preconnect (bad request)");
-            reject();
-          }
-        );
       } else {
-        console.log("no token in localstorage");
-        reject();
+        reject("No token stored");
       }
     });
   }
 
   connect(email, password) {
-    password = Md5.hashStr(password);
-
-    const url = "http://34.77.176.92/users/connect";
+    
     const body = {
-      "email": email,
-      "password": password
+      'email': email,
+      'password': Md5.hashStr(password)
     }
 
     return new Promise((resolve, reject) => {
+      this.send(this.KW_url_connect, null, body, HttpRequestType.POST)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+    });
+
+    /*return new Promise((resolve, reject) => {
         this.httpClient.post(url, body).subscribe(
           (response) => {
             console.log(response);
@@ -72,26 +106,31 @@ export class SessionService {
           }
         );
       }
-    );
-  }
-
-  disconnect() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('username');
-    localStorage.removeItem('email');
+    );*/
   }
 
   register(username, email, password) {
-    var hashPassword = Md5.hashStr(password);
 
-    const url = "http://34.77.176.92/users/add";
-    const parameters = {
-      "name": username,
-      "email": email,
-      "password": hashPassword
+    const body = {
+      'name': username,
+      'email': email,
+      'password': Md5.hashStr(password)
     }
 
     return new Promise((resolve, reject) => {
+      this.send(this.KW_url_register, null, body, HttpRequestType.POST)
+      .then((response) => {
+        localStorage.setItem(this.KW_token, response['token']);
+        this.setUsername(response['username']);
+        this.setEmail(response['email']);
+        resolve(response);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+    });
+
+    /*return new Promise((resolve, reject) => {
       this.httpClient.post(url, parameters).subscribe(
         (response) => {
           let token = response['token'];
@@ -108,16 +147,26 @@ export class SessionService {
           reject();
         }
       );
-    });
+    });*/
   }
 
   update(newUserName, newEmail, newPassword) {
+
+    return new Promise((resolve, reject) => {
+      let token = localStorage.getItem(this.KW_token);
+      if(token !== null) {
+
+      } else {
+
+      }
+    });
+
     return new Promise((resolve, reject) => {
       let token = localStorage.getItem('token');
       if(token != null) {
         const url = "http://34.77.176.92/users/";
         const header = new HttpHeaders({
-          'Authorization': "Bearer " + token
+          'Authorization': "bearer " + token
         });
         const body = {
           'name': newUserName,
@@ -149,15 +198,27 @@ export class SessionService {
     }); 
   }
 
-  getUsername() : string {
-    let username = localStorage.getItem('username');
+  disconnect() {
+    localStorage.removeItem(this.KW_token);
+    localStorage.removeItem(this.KW_username);
+    localStorage.removeItem(this.KW_email);
+  }
+
+  public getUsername() : string {
+    let username = localStorage.getItem(this.KW_username);
     return (username !== null) ? username : null;
   }
-  getEmail() : string {
-    let email = localStorage.getItem('email');
+  private setUsername(username) {
+    localStorage.setItem(this.KW_username, username);
+  }
+  public getEmail() : string {
+    let email = localStorage.getItem(this.KW_email);
     return (email !== null) ? email : null;
   }
-  isConnected() : boolean {
-    return (localStorage.getItem('token') !== null);
+  private setEmail(email) {
+    localStorage.setItem(this.KW_email, email);
+  }
+  public isConnected() : boolean {
+    return (localStorage.getItem(this.KW_token) !== null);
   }
 }
